0aa1147924c0789900acbeb6b16f5303d8eeb269
Rassa95 <39536503+Rassa95@users.noreply.github.com>
2019-02-02 18:25:05 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Apuntar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apuntar : MonoBehaviour {
+
+
+    public GameObject cadera;
+    public GameObject apuntando;
+
+	void Update () {
+
+        if (Input.GetMouseButton(1))
+        {
+            cadera.SetActive(false);
+            apuntando.SetActive(true);
+
+        } else
+        {
+            cadera.SetActive(true);
+            apuntando.SetActive(false);
+        }
+	}
+}
